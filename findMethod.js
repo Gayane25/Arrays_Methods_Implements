8038f6b511098a4201cb682d6ee5c975014e7504
Gayane25 <gayabennu@gmail.com>
2021-12-10 21:25:00 +0400

    feat:solved by using callback

--- findMethod.js
+++ findMethod.js
@@ -1,3 +1,15 @@
 'use strict';
 //Implement these array methods
 //find()
+
+const ages = [3, 10, 18, 20];
+const checkAge = findMethod(ages, age => age > 18);
+console.log(checkAge);
+function findMethod(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i)) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
