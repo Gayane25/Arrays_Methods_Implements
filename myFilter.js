ab175b8201364734a56322f60f0b2eba39276c9c
Gayane25 <gayabennu@gmail.com>
2021-12-10 18:38:34 +0400

    feat: resolved with callback function

--- myFilter.js
+++ myFilter.js
@@ -1,3 +1,16 @@
 'use strict';
 //Implement these array methods
 //filter method
+const array1 = [1, 4, 9, 16];
+const tofilter = myFilter(array1, x => x > 4);
+console.log(tofilter);
+
+function myFilter(arr, cb) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i, arr)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
