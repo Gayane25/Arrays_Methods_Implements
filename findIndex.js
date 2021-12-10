08082ad7193e74f9e1ee750bb9a44d0df4cc2e91
Gayane25 <gayabennu@gmail.com>
2021-12-10 22:05:18 +0400

    feat: solved with callback function

--- findIndex.js
+++ findIndex.js
@@ -1,2 +1,16 @@
 'use strict';
 //Implement these array methods
+// findIndex()
+
+const array1 = [5, 12, 8, 130, 44];
+const isLargeNumber = findIndex(array1, el => el > 13);
+console.log(isLargeNumber);
+
+function findIndex(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i)) {
+      return i;
+    }
+  }
+  return -1;
+}
