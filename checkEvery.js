cd863bd7154645397f6276c4c3c7a32d71d033e0
Gayane25 <gayabennu@gmail.com>
2021-12-10 20:27:48 +0400

    feat: implement Every() method with callback

--- checkEvery.js
+++ checkEvery.js
@@ -1,3 +1,15 @@
 //Implement these array methods
 //Every();
 'use strict';
+
+const array1 = [2, 10, 12, 20];
+const isEven = checkEvery(array1, el => el % 2 === 0);
+console.log(isEven);
+function checkEvery(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i, arr)) {
+      return true;
+    }
+    return false;
+  }
+}
