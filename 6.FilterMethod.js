70bafb216de66a6c9cea5ac4c9336c6fbf39f636
Gayane25 <gayabennu@gmail.com>
2021-12-10 23:54:52 +0400

    feat:solved with filter method

--- 6.FilterMethod.js
+++ 6.FilterMethod.js
@@ -4,3 +4,7 @@
 const arr = [1, 2, 'a', true, {}, undefined, 55, false, 'hi', null,
 166, NaN]
 // [1, 2, 55, 166]*/
+
+const arr = [1, 2, 'a', true, {}, undefined, 55, false, 'hi', null, 166, NaN];
+let arr2 = arr.filter(value => typeof value === 'number');
+console.log(arr2);
