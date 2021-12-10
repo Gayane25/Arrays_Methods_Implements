cc4c439de5bc7016a18d3b3e649f1cee0d2a9b78
Gayane25 <gayabennu@gmail.com>
2021-12-10 23:53:45 +0400

    feat: added strict mode and task ticket incomment

---
+++ 6.FilterMethod.js
@@ -0,0 +1,6 @@
+'use strict';
+/*Given an array, return a new array that only includes the numbers.
+``` javascript
+const arr = [1, 2, 'a', true, {}, undefined, 55, false, 'hi', null,
+166, NaN]
+// [1, 2, 55, 166]*/
