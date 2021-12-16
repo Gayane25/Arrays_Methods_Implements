e46b286940bf5552d3b6f78470b0d370e10c45bc
Gayane25 <gayabennu@gmail.com>
2021-12-16 16:25:27 +0400

    feat: solved with function, loop and condition

--- mySlice.js
+++ mySlice.js
@@ -1,2 +1,15 @@
 'use strict';
 // .slice()implement
+
+const mySlice = (arr, param1, param2) => {
+  if (typeof param2 === 'undefined' || param2 > arr.length) {
+    param2 = arr.length;
+  }
+  let newArr = [];
+  for (let i = param1; i < param2; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+
+console.log(mySlice([3, 5, 10, 15, 16, 13], 1, 3));
