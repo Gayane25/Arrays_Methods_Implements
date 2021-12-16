dcfa8b5bd0d40450972bb2fac8fa18fdb93d1eb8
Gayane25 <gayabennu@gmail.com>
2021-12-16 16:34:06 +0400

    feat:solved with push method

--- mySplice.js
+++ mySplice.js
@@ -1,2 +1,23 @@
 'use strict';
 // .splice() implement
+
+const mySplice = (arr, startIndex, deleteCount, ...newElem) => {
+  let beforeStartIndex = [];
+  let afterStartIndex = [];
+  let removeElements = [];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i < startIndex) {
+      beforeStartIndex.push(arr[i]);
+    } else if (i >= startIndex && count !== deleteCount) {
+      removeElements.push(arr[i]);
+      count++;
+    } else {
+      afterStartIndex.push(arr[i]);
+    }
+  }
+  arr.length = 0;
+  arr = arr.concat(beforeStartIndex, ...newElem, afterStartIndex);
+  console.log(removeElements, arr);
+};
+mySplice([4, 8, 9, 0, 5], 2, 2, 5, 6);
